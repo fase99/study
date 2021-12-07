a5d188082143b7364e04dea8f3f5cabec48f81af
fase99 <89035508+fase99@users.noreply.github.com>
2021-12-07 12:53:39 -0400

    Update Solucion.cpp

--- Solucion.cpp
+++ Solucion.cpp
@@ -19,12 +19,55 @@ vector<string> separador(const char *str, char c = ' '){
 }
 
 
-bool solucion(string revista, string carta){
+void solucion(string revista, string carta){
 
 vector<string> auxR = separador(revista.c_str());
 vector<string> auxC = separador(carta.c_str());
 
+map<int,string> map;
 
+int largoC = auxC.size();
+    for(int i=0; i<largoC; i++){
+        int id = i+1;
+        map[id] = auxC[i];
+
+        auxC.erase(auxC.begin() + i);
+    }
+
+int largoR = auxR.size();
+
+    auto itr = map.begin();
+    for(itr = map.begin(); itr!=map.end();itr++){
+        for(int i=0; i<largoR; i++){
+
+            if(itr->second == auxR[i]){
+
+                cout<<"palabra = "<<itr->second<<endl;
+                auxR.erase(auxR.begin()+i);
+                //eliminar elemento del mapa
+
+            }
+            else{
+                cout<<"no hay"<<endl;
+            }
+        }
+
+    }
+/* 
+    if(map.empty()){
+        return true;
+    }
+    else{
+        return false;
+    } */
+
+    /* for(int i=0; i<auxC.size(); i++){
+        cout<<auxC[i]<<endl;
+    }
+    */
+    for(itr = map.begin(); itr!=map.end(); itr++){
+        cout<<itr->first<<" - "<<itr->second<<endl;
+    } 
 
 }
 
@@ -32,15 +75,26 @@ vector<string> auxC = separador(carta.c_str());
 int main(){
 
 
-string revista;
-string carta;
+string revista = "Necesito que Dios entre a mi vida como un arma";
+string carta = "Necesito auto arma Dios";
 
-cout<<"Ingrese el texto de su carta: "; cin>>carta;
-cout<<"Ingrese el texto de la revista: "; cin>>revista;
 
 	vector<string> auxC = separador(carta.c_str());
 	vector<string> auxR = separador(revista.c_str());
 
+    
+    solucion(revista, carta);
+
+
+   /*  for(int i = 0; i<auxC.size(); i++){
+        cout<<auxC[i]<<endl;
+    }
+
+    cout<<"---------------------------------------"<<endl<<endl;
+
+    for(int i=0; i<auxR.size(); i++){
+        cout<<auxR[i]<<endl;
+    } */
 
 return 0;
 }
